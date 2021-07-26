ab04b2936a26ac7ee61f26771328de5ccf3d1756
Derek Oh <D.O@Dereks-MacBook-Air.local>
2021-07-26 18:53:34 -0400

    tests

---
+++ src/components/Card.js
@@ -0,0 +1,11 @@
+import './index.css'
+export default function Card(){
+
+   return (
+    <div class="card">
+        
+        <p>nickname -- species</p>
+    <button class="edit"> Edit </button>
+</div>
+   )
+}
